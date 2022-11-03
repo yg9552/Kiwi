c1eab38d004b72092b98964f81ba4ba98b9008bd
Joseph <joseph0417@naver.com>
2022-11-03 17:33:19 +0900

    암호화 util 추가

---
+++ src/main/java/com/nextrip/common/util/UtilSecurity.java
@@ -0,0 +1,32 @@
+package com.nextrip.common.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class UtilSecurity {
+	
+	public static String encryptSha256(String text) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(text.getBytes());
+
+        return bytesToHex(md.digest());
+    }
+
+    private static  String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }	
+    
+    public static String getSessionSeqCore(HttpServletRequest httpServletRequest) {
+		HttpSession httpSession =  httpServletRequest.getSession();
+		String rtSeq = (String) httpSession.getAttribute("sessSeq");
+		return rtSeq;
+	}
+
+}
